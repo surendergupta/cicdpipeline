f3c834f167e227190c1efd702d5bea23b88b9d1c
Surender Gupta <gupta.surender.1990@gmail.com>
2023-12-05 17:45:02 +0530

    Update gitcheckrepo.py

--- bash_script/gitcheckrepo.py
+++ bash_script/gitcheckrepo.py
@@ -1,9 +1,8 @@
-from git import Repo, GitCommandError
+from git import Repo, GitCommandError, InvalidGitRepositoryError
+import os
 import subprocess
 
-def check_latest_commit(repo_path):
-    repo = Repo(repo_path)
-
+def check_latest_commit(repo):
     # Fetch the latest changes from the remote repository
     origin = repo.remote(name='origin')
     origin.fetch()
@@ -11,33 +10,31 @@ def check_latest_commit(repo_path):
     # Get the latest commit
     latest_commit = repo.head.commit
     return latest_commit
+
 def deploy_and_restart():
     # Replace this command with the actual command to deploy and restart your application
     deployment_command = "echo 'Deploying and restarting application...'"
-
-
+    
     # Run the deployment command
-    subprocess.run(['sh', '/deploy.sh'], shell=True)
+    subprocess.run(['python3', '/home/ubuntu/cicdpipeline/bash_script/deploy.sh'], shell=True)
 
 def devmain():
     repo_url = 'https://github.com/surendergupta/cicdpipeline'
-    repo_path = 'C:\\Users\\PriyaJi\\OneDrive\\Desktop\\cicdpipeline'  # Provide the local path where you want to clone the repository
+    repo_path = '/var/www/html/cicdpipeline'  # Provide the local path where you want to clone the repository
 
     try:
         repo = Repo(repo_path)
-    except NoSuchPathError:
+        print("Repository exists. Pulling the latest changes...")
+        repo.remotes.origin.pull()
+    except InvalidGitRepositoryError:
         try:
             repo = Repo.clone_from(repo_url, repo_path)
+            print("Cloning the repository for the first time...")
         except GitCommandError as e:
-            print(f"Error: {e}")
-            # Handle the error, perhaps the repository does not exist or there's an issue with the URL
-
-    # Now you can work with the repo object
-    # latest_commit = check_latest_commit(repo_path)
-    # print(f"Latest Commit SHA: {latest_commit.hexsha}")
-
-    prev_commits = list(repo.iter_commits(all=True, max_count=10))
-    latest_commit = prev_commits[0]
+            print(f"Error cloning the repository: {e}")
+            return
+	
+	latest_commit = check_latest_commit(repo)
     
     # Check if the latest commit is different from the currently deployed commit
     # You may need to modify this condition based on your deployment strategy
